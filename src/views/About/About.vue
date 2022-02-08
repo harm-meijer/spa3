5489dfd44ba1fa97f5fe3e22ed14c6e14a88f546
Felipe Lemos <felipe.lemos@commercetools.com>
2022-02-08 10:27:59 +0100

    Added SCSS - About

--- src/views/About/About.vue
+++ src/views/About/About.vue
@@ -1,11 +1,9 @@
-<!-- <style src="./About.scss" lang="scss" scoped></style> -->
+<style src="./About.scss" lang="scss" scoped></style>
 <i18n src="./About.txt"></i18n>
 <script src="./About.js"></script>
-
 
 <template>
   <h1 class="test">About</h1>
   <!-- this should say thankyou in the app language -->
   <!-- <h2>{{ $t('thankYou') }}</h2> -->
 </template>
-
