f3ca1670c2cf51a01763607fd5bfe137f6d60440
Felipe Lemos <felipe.lemos@commercetools.com>
2022-02-10 11:46:08 +0100

    Cart changes

--- src/views/Shop/Root/Cart/Cart.js
+++ src/views/Shop/Root/Cart/Cart.js
@@ -1,5 +1,6 @@
 import useCart from '../../../../../composition/useCart';
 import useLocale from '../../../../../composition/useLocale';
+import { useI18n } from 'vue-i18n';
 
 export default {
   name: 'Cart',
@@ -8,10 +9,15 @@ export default {
     const { cart, loading, error } = useCart({
       locale: locale.value,
     });
+    const t = useI18n({
+      inheritLocale: true,
+      useScope: 'local',
+    });
     return {
       cart,
       loading,
       error,
+      ...t,
     };
   },
 };
