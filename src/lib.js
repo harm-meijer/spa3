97276db1a07d07cc6f861ac011fddac2455743b5
Harm Meijer <harm.meijer@commercetools.com>
2022-02-11 19:37:37 +0100

    spell check

--- src/lib.js
+++ src/lib.js
@@ -3,7 +3,7 @@ export const move = (router, route, params, fn) =>
     ...route,
     params,
   });
-//in React you can just return the obervable
+//in React you can just return the observable
 //  because React does not usually have observable
 //  in React it will be: export const getValue = (observable) => observable;
 export const getValue = (observable) => {
