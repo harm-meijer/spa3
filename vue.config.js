9f838fa0505ca2595b519425d98692c6fe3da1e4
Felipe Lemos <felipe.lemos@commercetools.com>
2022-02-14 11:09:30 +0100

    Add alias to resolve paths.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+const path = require('path');
+const PROJECT_PATH = `src/${
+  process.env.PROJECT_PATH || ''
+}`;
+
 module.exports = {
   pluginOptions: {
     i18n: {
@@ -7,6 +12,36 @@ module.exports = {
       runtimeOnly: false,
       compositionOnly: false,
       fullInstall: true,
+    },
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        Cart: path.resolve(
+          __dirname,
+          `${PROJECT_PATH}/views/Shop/Root/`
+        ),
+        Home: path.resolve(
+          __dirname,
+          `${PROJECT_PATH}/views/Home`
+        ),
+        Product: path.resolve(
+          __dirname,
+          `${PROJECT_PATH}/views/Product`
+        ),
+        Products: path.resolve(
+          __dirname,
+          `${PROJECT_PATH}/views/Products`
+        ),
+        About: path.resolve(
+          __dirname,
+          `${PROJECT_PATH}/views/About`
+        ),
+        Components: path.resolve(
+          __dirname,
+          `${PROJECT_PATH}/components`
+        ),
+      },
     },
   },
 };
