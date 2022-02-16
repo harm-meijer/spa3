40f0eb8a6173623d941ca1becf576b5032da5cd9
Felipe Lemos <felipe.lemos@commercetools.com>
2022-02-16 15:31:16 +0100

    Use scss from _common.scss

--- src/presentation/fashion/Home/Home.vue
+++ src/presentation/fashion/Home/Home.vue
@@ -1,6 +1,9 @@
 <script src="./Home.js" />
+<style src="./Home.scss" lang="scss"></style>
 
 <template>
-  <h1>hello from presentation</h1>
+  <div>
+    <h1>hello from presentation</h1>
+  </div>
   <Cart />
 </template>
