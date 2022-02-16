22d15647c623afff09224be174d43adcab5617bb
Harm Meijer <harm.meijer@commercetools.com>
2022-02-16 14:24:08 +0100

    fix page size

--- src/constants.js
+++ src/constants.js
@@ -3,4 +3,4 @@ export const LOCATION = 'LOCATION';
 export const ACCESS_TOKEN = 'ACCESS_TOKEN';
 export const REFRESH_TOKEN = 'REFRESH_TOKEN';
 export const ALL = 'all';
-export const DEFAULT_PAGE_SIZE = 'DEFAULT_PAGE_SIZE';
+export const DEFAULT_PAGE_SIZE = 10;
