062dcd600c1e2bdf7835f6d22073d109a9acbd90
Felipe Lemos <felipe.lemos@commercetools.com>
2022-02-17 12:03:52 +0100

    Remove unused imports for locale

--- src/main.js
+++ src/main.js
@@ -4,10 +4,7 @@ import App from './App.vue';
 import { apolloClient } from './apollo';
 import router from './router';
 import VueGoogleMaps from '@fawmi/vue-google-maps';
-//import useLocale from '../composition/useLocale';
 import i18n from './i18n';
-
-//const { locale } = useLocale();
 
 const app = createApp({
   setup() {
