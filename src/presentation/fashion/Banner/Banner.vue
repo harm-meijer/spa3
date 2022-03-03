5f34a6efe396d9930da89d153b020994e6b38b2f
Felipe Lemos <felipe.lemos@commercetools.com>
2022-03-03 16:17:28 +0100

    Fade transition

--- src/presentation/fashion/Banner/Banner.vue
+++ src/presentation/fashion/Banner/Banner.vue
@@ -2,17 +2,31 @@
 <i18n src="./Banner.txt" lang="yaml"></i18n>
 
 <template>
-  <Slide
+  <div
     class="slider-area"
     v-for="slide in slides"
     :key="slide.id"
   >
-    <BannerSlide
-      :bannerImage="slide.bannerImage"
-      :h3Message="t(slide.h3Message)"
-      :h1Message="t(slide.h1Message)"
-      :bttnText="t(slide.bttnText)"
-    >
-    </BannerSlide>
-  </Slide>
+    <transition name="slide-fade" appear>
+      <BannerSlide
+        :bannerImage="slide.bannerImage"
+        :h3Message="t(slide.h3Message)"
+        :h1Message="t(slide.h1Message)"
+        :bttnText="t(slide.bttnText)"
+      >
+      </BannerSlide>
+    </transition>
+  </div>
 </template>
+
+<style>
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+  transition: opacity 2s ease;
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  opacity: 0;
+}
+</style>
