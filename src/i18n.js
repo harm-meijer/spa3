2404f6d8cceeee0fd2137014698ada205cf558eb
Harm Meijer <harm.meijer@commercetools.com>
2022-03-18 13:01:06 +0100

    fix date time format due to vue team not casing their prop names correctly

--- src/i18n.js
+++ src/i18n.js
@@ -5,7 +5,8 @@ const i18n = createI18n({
   legacy: false,
   globalInjection: true,
   numberFormats: config.formats.number,
-  dateTimeFormats: config.formats.datetime,
+  //bug in vue i18n, wrong casing for this prop
+  datetimeFormats: config.formats.datetime,
 });
 
 export default i18n;
